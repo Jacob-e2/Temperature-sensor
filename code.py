7be229fe0fe03f84279fefcade9a54260f038cc6
Jacob-e2 <69513671+Jacob-e2@users.noreply.github.com>
2023-03-15 15:05:22 +0000

    Update code.py
    Added comments

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import adafruit_ahtx0
 import busio
 from adafruit_datetime import datetime
 
-i2c = busio.I2C(scl=board.GP1, sda=board.GP0) 
+i2c = busio.I2C(scl=board.GP1, sda=board.GP0) #Configures which ports on the microcontroller are being used.
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
 settings = open("/Settings.txt", "r")
@@ -13,7 +13,7 @@ print(interval)
 
 while True:   #Main while loop ensures that the temeprature recordings are repeated.
     temperature = sensor.temperature
-    log = open("/Temperature.txt", "a")
+    log = open("/Temperature.txt", "a") #Writes he temperature data to the log.
     log.write(str(temperature) + "\n")
     log.close()
     time.sleep(int(interval))
