bbf4aad04a2c8225fe003d4eadea9be13de9e04a
Jacob-e2 <69513671+Jacob-e2@users.noreply.github.com>
2023-03-15 14:47:06 +0000

    Added comments

--- weather.py
+++ weather.py
@@ -2,11 +2,11 @@ from requests import get
 import ipinfo
 import requests
 
-def ip_address():
+def ip_address(): #Subprogram to obtain the users IP address
   ip = get('https://api.ipify.org').text
   return ip
 
-def ip_location(ip):
+def ip_location(ip): #Subprogram to find the location linked to the users IP
   access_token = 'abe318631b66d7'
   handler = ipinfo.getHandler(access_token)
   details = handler.getDetails(ip)
@@ -16,13 +16,13 @@ def ip_location(ip):
   print(city)
   return location
 
-def weather(location):
+def weather(location): #Subprogram to query the weather API.
   params = {
     'access_key': '63d3a62502dd2f601a7850aea8ff8259',
     'query': location
   }
 
-  api_result = requests.get('http://api.weatherstack.com/current', params)
+  api_result = requests.get('http://api.weatherstack.com/current', params) #Processes the JSON file
   api_response = api_result.json()
   print(api_result)
   print(api_response['current']['temperature'])
